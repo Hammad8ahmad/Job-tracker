2a3c5bc335cad81e1544af671d5cdfb7ea49d778
Hammad8ahmad <hammadahmdd8@gmail.com>
2024-09-07 11:48:14 +0100

    small styling change

--- frontend/src/components/EmptyState.js
+++ frontend/src/components/EmptyState.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const EmptyState = ({ filteredJobs }) => {
   return (
-    <div className="  flex items-center justify-center text-black py-10 px-5  text-6xl mt-10">
+    <div className="  flex items-center justify-center text-black dark:text-white py-10 px-5  text-6xl mt-10">
       <div>You currently have {filteredJobs.length} jobs.</div>
     </div>
   );
