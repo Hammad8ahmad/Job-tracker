fe63cfde7b2c3e0e1cbd12c028840d0666e12d51
Hammad8ahmad <hammadahmdd8@gmail.com>
2024-12-31 14:17:01 +0000

    testing cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const jobRoutes = require("./Routes/JobRoutes");
 
 // Middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-tracker-io.vercel.app",
+  })
+);
 app.use(express.json()); // req.body
 
 app.use((req, res, next) => {
